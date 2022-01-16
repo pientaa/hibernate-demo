4c5229fd4a8d07bee8036559d3b28c6a1f772b74
Łukasz Pięta <pieta.lukasz98@gmail.com>
2022-01-16 08:56:06 +0100

    Initial commit

---
+++ src/test/kotlin/com/pientaa/hibernatedemo/HibernateDemoApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.pientaa.hibernatedemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HibernateDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
