a67d644b4fa3c24b6828de71e5aa635c8fda8eb7
Łukasz Pięta <pieta.lukasz98@gmail.com>
2022-10-23 00:20:07 +0200

    Remove unnecessary code

--- src/main/kotlin/com/pientaa/hibernatedemo/domain/PostService.kt
+++ src/main/kotlin/com/pientaa/hibernatedemo/domain/PostService.kt
@@ -11,20 +11,16 @@ class PostService(
     private val postRepository: PostRepository
 ) {
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
     fun save(post: Post) = postRepository.save(post).id!! //should be some custom exception thrown instead
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
     fun addComment(comment: String, postId: Long) {
         postRepository.getReferenceById(postId)
             .apply { addComment(comment) }
             .let { postRepository.save(it) }
     }
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
     fun deleteAll() = postRepository.deleteAll()
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
     fun deleteById(postId: Long) = postRepository.deleteById(postId)
 
     fun getPostWithComments(postId: Long): Post = postRepository.getReferenceById(postId)
