a25e1849d022b46cd83aa1bcedfe8c265dd103dc
Łukasz Pięta <pieta.lukasz98@gmail.com>
2022-10-28 16:50:55 +0200

    Data classes configured to be entities

--- src/main/kotlin/com/pientaa/hibernatedemo/entity/Post.kt
+++ src/main/kotlin/com/pientaa/hibernatedemo/entity/Post.kt
@@ -1,5 +1,6 @@
 package com.pientaa.hibernatedemo.entity
 
+import org.hibernate.Hibernate
 import javax.persistence.CascadeType
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
@@ -21,5 +22,20 @@ data class Post(
         comments.add(
             PostComment(content = comment, post = this)
         )
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+        other as Post
+
+        return id != null && id == other.id
+    }
+
+    override fun hashCode(): Int = javaClass.hashCode()
+
+    @Override
+    override fun toString(): String {
+        return this::class.simpleName + "(id = $id , title = $title , content = $content )"
     }
 }
