0c43b90a7d9ac79347e0dad26461f58443690365
Łukasz Pięta <pieta.lukasz98@gmail.com>
2023-03-11 23:05:01 +0100

    Stage 1 - simple PostEntity, no relationship

--- src/main/kotlin/com/pientaa/hibernatedemo/PostEntity.kt
+++ src/main/kotlin/com/pientaa/hibernatedemo/PostEntity.kt
@@ -4,8 +4,10 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 @Entity
+@Table(name = "post")
 class PostEntity(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
