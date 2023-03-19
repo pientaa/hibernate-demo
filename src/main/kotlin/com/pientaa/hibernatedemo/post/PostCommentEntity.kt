c05cf63a03a5a07c99b2af49634c522f612aca54
Łukasz Pięta <pieta.lukasz98@gmail.com>
2023-03-19 13:08:11 +0100

    How not to Hibernate - stage 9

---
+++ src/main/kotlin/com/pientaa/hibernatedemo/post/PostCommentEntity.kt
@@ -0,0 +1,17 @@
+package com.pientaa.hibernatedemo.post
+
+import com.pientaa.hibernatedemo.author.AuthorEntity
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "post_comment")
+class PostCommentEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null,
+    @Column(nullable = false)
+    var content: String,
+
+    @ManyToOne(optional = false)
+    val author: AuthorEntity
+)
