c20635d090a4a0f4833631464bf220e60ba18ae5
Łukasz Pięta <pieta.lukasz98@gmail.com>
2023-03-19 07:46:11 +0100

    How not to Hibernate - stage 8
    (cherry picked from commit f2ec6cae5f8789909bf253d89d40276f0b99c31c)

--- src/main/kotlin/com/pientaa/hibernatedemo/author/ContactInfo.kt
+++ src/main/kotlin/com/pientaa/hibernatedemo/author/ContactInfo.kt
@@ -1,10 +1,14 @@
 package com.pientaa.hibernatedemo.author
 
+import jakarta.persistence.Column
 import jakarta.persistence.Embeddable
 
 @Embeddable
 data class ContactInfo(
+    @Column(nullable = false)
     var address: String,
+    @Column(nullable = false)
     var phone: String,
+    @Column(nullable = false)
     var email: String,
 )
