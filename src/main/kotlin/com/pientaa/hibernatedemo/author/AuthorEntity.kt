d4ffa4dfe31440c197393a028730d13f7830f1c1
Łukasz Pięta <pieta.lukasz98@gmail.com>
2023-03-25 09:27:11 +0100

    How not to Hibernate - constraints fixes

--- src/main/kotlin/com/pientaa/hibernatedemo/author/AuthorEntity.kt
+++ src/main/kotlin/com/pientaa/hibernatedemo/author/AuthorEntity.kt
@@ -8,9 +8,11 @@ class AuthorEntity(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
+    @Column(nullable = false)
     var firstName: String,
+    @Column(nullable = false)
     var lastName: String,
 
-    @OneToOne(cascade = [CascadeType.ALL], orphanRemoval = true)
-    val contactInfo: ContactInfoEntity
+    @OneToOne(cascade = [CascadeType.ALL], orphanRemoval = true, optional = false)
+    var contactInfo: ContactInfoEntity
 )
