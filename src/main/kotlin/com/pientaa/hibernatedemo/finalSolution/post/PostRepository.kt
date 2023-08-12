f561857080cd77fd22535fcd4ace078e03536bc6
Łukasz Pięta <pieta.lukasz98@gmail.com>
2023-08-12 14:59:28 +0200

    Fix after repackaging

--- src/main/kotlin/com/pientaa/hibernatedemo/finalSolution/post/PostRepository.kt
+++ src/main/kotlin/com/pientaa/hibernatedemo/finalSolution/post/PostRepository.kt
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository
 interface PostRepository : JpaRepository<Post, Long> {
     @Query(
         """
-        select new com.pientaa.hibernatedemo.post.PostCommentDto(p.id, p.title, pc.id, pc.content) 
+        select new com.pientaa.hibernatedemo.finalSolution.post.PostCommentDto(p.id, p.title, pc.id, pc.content) 
         from Post p left join PostComment pc on p.id = pc.post.id
         """
     )
